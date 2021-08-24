bd5535de30372ea353986cfe7ebf0ac5164fe11d
Anastasiia.Birillo <nbirillo@mail.ru>
2021-08-24 17:01:20 +0300

    Add HYPERSTYLE_RUNNER_PATH env variable

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,7 +1,11 @@
+import os
 import sys
 from pathlib import Path
 
 MAIN_FOLDER = Path(__file__)
 HYPERSTYLE_PATH = f'{MAIN_FOLDER.parent.parent}/main/venv/src'
 sys.path.append(HYPERSTYLE_PATH)
-HYPERSTYLE_RUNNER_PATH = Path(HYPERSTYLE_PATH) / 'hyperstyle/src/python/review/run_tool.py'
+if os.getenv('HYPERSTYLE_RUNNER_PATH') is not None:
+    HYPERSTYLE_RUNNER_PATH = os.getenv('HYPERSTYLE_RUNNER_PATH')
+else:
+    HYPERSTYLE_RUNNER_PATH = Path(HYPERSTYLE_PATH) / 'hyperstyle/src/python/review/run_tool.py'
