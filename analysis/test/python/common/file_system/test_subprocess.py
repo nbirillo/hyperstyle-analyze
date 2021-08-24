e7461f897a992ac77b49e6670dd250e537a18e34
Anastasiia.Birillo <nbirillo@mail.ru>
2021-08-24 10:44:20 +0300

    Reorganize file structure

---
+++ analysis/test/python/common/file_system/test_subprocess.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+import pytest
+from hyperstyle.src.python.review.application_config import LanguageVersion
+from hyperstyle.src.python.review.common.file_system import create_file, get_content_from_file
+from hyperstyle.src.python.review.common.subprocess_runner import run_in_subprocess
+from evaluation.evaluation_config import EvaluationConfig
+
+from common import FILE_SYSTEM_DATA_FOLDER
+from evaluation.testing_config import get_testing_arguments
+
+INPUT_DATA = [
+    ('in_1.java', LanguageVersion.JAVA_11),
+    ('in_2.py', LanguageVersion.PYTHON_3),
+]
+
+
+def inspect_code(config: EvaluationConfig, file: str, language: LanguageVersion, history: Optional[str] = None) -> str:
+    command = config.build_command(file, language.value, history)
+    return run_in_subprocess(command)
+
+
+@pytest.mark.parametrize(('test_file', 'language'), INPUT_DATA)
+def test_synthetic_files(test_file: str, language: LanguageVersion):
+    input_file = FILE_SYSTEM_DATA_FOLDER / test_file
+    test_args = get_testing_arguments(to_add_traceback=True, to_add_tool_path=True)
+    config = EvaluationConfig(test_args)
+
+    expected_output = inspect_code(config, input_file, language)
+
+    input_code = get_content_from_file(Path(input_file))
+    actual_file = next(create_file(FILE_SYSTEM_DATA_FOLDER / f'actual_file{language.extension_by_language().value}',
+                                   input_code))
+
+    actual_output = inspect_code(config, actual_file, language)
+    os.remove(actual_file)
+
+    assert actual_output == expected_output
---
+++ analysis/test/python/evaluation/test_data_path.py
@@ -0,0 +1,15 @@
+import pytest
+from evaluation.evaluation_config import EvaluationConfig
+from evaluation.evaluation_run_tool import get_solutions_df, inspect_solutions_df
+
+from evaluation import XLSX_DATA_FOLDER
+from evaluation.testing_config import get_testing_arguments
+
+
+def test_incorrect_data_path():
+    with pytest.raises(FileNotFoundError):
+        testing_arguments_dict = get_testing_arguments(to_add_traceback=True, to_add_tool_path=True)
+        testing_arguments_dict.solutions_file_path = XLSX_DATA_FOLDER / 'do_not_exist.xlsx'
+        config = EvaluationConfig(testing_arguments_dict)
+        lang_code_dataframe = get_solutions_df(config.extension, config.solutions_file_path)
+        assert inspect_solutions_df(config, lang_code_dataframe)
