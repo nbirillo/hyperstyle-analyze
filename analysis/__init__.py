57808b27486565fe38148a44a61368fc982beaf2
Anastasiia.Birillo <nbirillo@mail.ru>
2021-08-24 17:53:50 +0300

    Fix

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -2,6 +2,6 @@ import sys
 from pathlib import Path
 
 MAIN_FOLDER = Path(__file__)
-HYPERSTYLE_PATH = f'{MAIN_FOLDER.parent.parent.parent}/main/venv/src'
+HYPERSTYLE_PATH = f'{MAIN_FOLDER.parent.parent}/main/venv/src'
 sys.path.append(HYPERSTYLE_PATH)
 HYPERSTYLE_RUNNER_PATH = Path(HYPERSTYLE_PATH) / 'hyperstyle/src/python/review/run_tool.py'
