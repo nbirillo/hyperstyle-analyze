14e8620a3e1055570a56acf2de94bab998eb6d93
tiginamaria <tiginamaria@gmail.com>
2022-07-06 14:00:44 +0200

    Add test for read/write/append dataframe to exel/csv files

--- analysis/test/python/utils/df_utils/test_read_df.py
+++ analysis/test/python/utils/df_utils/test_read_df.py
@@ -1,20 +1,25 @@
 from pathlib import Path
+from typing import Optional, Tuple
 
 import pytest
 
-from analysis.src.python.evaluation.model.column_name import ColumnName
-from analysis.src.python.utils.df_utils import drop_duplicates, equal_df, read_df
-from analysis.src.python.evaluation.utils.args_util import get_in_and_out_list
+from analysis.src.python.utils.df_utils import read_df, write_df
 from analysis.test.python.utils import DF_UTILS_DATA_FOLDER
 
 RESOURCES_PATH = DF_UTILS_DATA_FOLDER / 'read_df'
 
-IN_AND_OUT_FILES = get_in_and_out_list(RESOURCES_PATH)
+INPUT_DATA = [
+    ('in_1.csv', (3, 4)),
+    ('in_2.xlsx', (3, 4)),
+    ('in_3.json', None)
+]
 
 
-@pytest.mark.parametrize(('in_file', 'out_file'), IN_AND_OUT_FILES)
-def test(in_file: Path, out_file: Path):
-    in_df = read_df(in_file)
-    out_df = read_df(out_file)
-    filtered_df = drop_duplicates(in_df, ColumnName.CODE.value)
-    assert equal_df(out_df, filtered_df)
+@pytest.mark.parametrize(('in_file', 'shape'), INPUT_DATA)
+def test(in_file: Path, shape: Optional[Tuple[int, int]]):
+    if shape is not None:
+        in_df = read_df(RESOURCES_PATH / in_file)
+        assert in_df.shape == shape
+    else:
+        with pytest.raises(ValueError):
+            read_df(RESOURCES_PATH / in_file)
