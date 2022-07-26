5549d1702b8676f4ae2eaf71e349d5d8a40b590c
tiginamaria <tiginamaria@gmail.com>
2022-07-26 18:30:41 +0200

    Add common evaluation for hyperskill and qodana

--- analysis/test/python/evaluation/hyperstyle/test_docker_evaluation.py
+++ analysis/test/python/evaluation/hyperstyle/test_docker_evaluation.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from analysis.src.python.evaluation.hyperstyle.evaluate import evaluate
+from analysis.src.python.evaluation.hyperstyle.evaluate import evaluate_hyperstyle
 from analysis.src.python.evaluation.hyperstyle.evaluation_config import HYPERSTYLE_DOCKER_PATH, HYPERSTYLE_TOOL_PATH, \
     HyperstyleEvaluationConfig
 from analysis.src.python.evaluation.utils.args_utils import get_in_and_out_list
@@ -14,7 +14,7 @@ RESOURCES_PATH = HYPERSTYLE_DIR_PATH / 'docker_evaluation'
 IN_AND_OUT_FILES = get_in_and_out_list(RESOURCES_PATH)
 
 
-# @pytest.mark.skip(reason="No docker inside CI container")
+@pytest.mark.skip(reason="No docker inside CI container")
 @pytest.mark.parametrize(('in_file', 'out_file'), IN_AND_OUT_FILES)
 def test_with_batching(in_file: Path, out_file: Path):
     in_df = read_df(in_file)
@@ -23,15 +23,14 @@ def test_with_batching(in_file: Path, out_file: Path):
                                                 tool_path=HYPERSTYLE_TOOL_PATH,
                                                 allow_duplicates=False,
                                                 with_all_categories=False,
-                                                new_format=True,
-                                                )
+                                                new_format=True)
 
-    inspected_df = evaluate(in_df, testing_config)
+    inspected_df = evaluate_hyperstyle(in_df, testing_config)
     out_df = read_df(out_file)
     assert equal_df(out_df, inspected_df)
 
 
-# @pytest.mark.skip(reason="No docker inside CI container")
+@pytest.mark.skip(reason="No docker inside CI container")
 @pytest.mark.parametrize(('in_file', 'out_file'), IN_AND_OUT_FILES)
 def test_without_batching(in_file: Path, out_file: Path):
     in_df = read_df(in_file)
@@ -40,9 +39,8 @@ def test_without_batching(in_file: Path, out_file: Path):
                                                 tool_path=HYPERSTYLE_TOOL_PATH,
                                                 allow_duplicates=False,
                                                 with_all_categories=False,
-                                                new_format=False,
-                                                )
+                                                new_format=False)
 
-    inspected_df = evaluate(in_df, testing_config)
+    inspected_df = evaluate_hyperstyle(in_df, testing_config)
     out_df = read_df(out_file)
     assert equal_df(out_df, inspected_df)
