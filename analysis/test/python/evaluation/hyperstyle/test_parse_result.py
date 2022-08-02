240af8c66525ab787ab23af1c91c92994229a621
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2022-08-02 15:35:40 +0200

    Tests for parsing results (#32)
    * Start common parts extraction
    
    * Implement qodana evaluation
    
    * Add common evaluation for hyperskill and qodana
    
    * Add qodana output
    
    * Fix tests
    
    * Fix tests
    
    * Remove old qodana evaluation script
    
    * Fix tests
    
    * Fix tmp file and deserialization from file
    
    * Fix flake8
    
    * Add tests for parsing hyperstyle results
    
    * Add qodana report parsing test
    
    * Add test utils to avoid code duplication in tests
    
    * Add test assertion message
    
    * Fix assertion
    
    * Fix after merge
    
    * Fix flake8

---
+++ analysis/test/python/evaluation/hyperstyle/test_parse_result.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+import pytest
+
+from analysis.src.python.data_analysis.model.column_name import SubmissionColumns
+from analysis.src.python.evaluation.hyperstyle.evaluate import parse_hyperstyle_new_format_result, \
+    parse_hyperstyle_result
+from analysis.src.python.evaluation.hyperstyle.model.report import HyperstyleReport
+from analysis.test.python.evaluation import HYPERSTYLE_DIR_PATH
+from analysis.test.python.evaluation.utils.evaluation_test_utils import run_evaluation_parsing_test
+
+RESOURCES_PATH = HYPERSTYLE_DIR_PATH / 'parse_result'
+NEW_FORMAT_RESOURCES_PATH = RESOURCES_PATH / 'new_format'
+OLD_FORMAT_RESOURCES_PATH = RESOURCES_PATH / 'old_format'
+
+NEW_FORMAT_DATA = [
+    ('java8_result.json', 3, [4, 6, 5]),
+    ('java11_result.json', 3, [1, 4, 0]),
+    ('js_result.json', 1, [3]),
+    ('python3_result.json', 3, [0, 0, 2]),
+    ('kotlin_result.json', 3, [1, 2, 2]),
+]
+
+
+def parse_hyperstyle_issues(df: pd.Series):
+    report = HyperstyleReport.from_json(df[SubmissionColumns.HYPERSTYLE_ISSUES.value])
+    return report.issues
+
+
+@pytest.mark.parametrize(('result_file', 'solutions_count', 'issues_count'), NEW_FORMAT_DATA)
+def test_new_format(result_file: Path, solutions_count: int, issues_count: List[int]):
+    run_evaluation_parsing_test(
+        result_path=NEW_FORMAT_RESOURCES_PATH / result_file,
+        parse_result=parse_hyperstyle_new_format_result,
+        get_result_issues=parse_hyperstyle_issues,
+        result_shape=solutions_count,
+        result_row_shapes=issues_count)
+
+
+OLD_FORMAT_DATA = [
+    ('java8_result.json', 5),
+    ('python3_result.json', 2),
+    ('kotlin_result.json', 2),
+]
+
+
+@pytest.mark.parametrize(('result_file', 'issues_count'), OLD_FORMAT_DATA)
+def test_old_format(result_file: Path, issues_count: int):
+    run_evaluation_parsing_test(
+        result_path=OLD_FORMAT_RESOURCES_PATH / result_file,
+        parse_result=lambda result_path: parse_hyperstyle_result(result_path).to_frame().T,
+        get_result_issues=parse_hyperstyle_issues,
+        result_shape=1,
+        result_row_shapes=[issues_count])
--- analysis/test/python/evaluation/test_xlsx_file_structure.py
+++ analysis/test/python/evaluation/test_xlsx_file_structure.py
@@ -2,10 +2,9 @@ import pytest
 
 from analysis import HYPERSTYLE_RUNNER_PATH
 from analysis.src.python.evaluation.hyperstyle.evaluate import evaluate_hyperstyle
-from analysis.src.python.evaluation.hyperstyle.evaluation_config import HyperstyleEvaluationConfig
 from analysis.src.python.utils.df_utils import read_df
-from analysis.src.python.utils.file_utils import create_directory, remove_directory
-from analysis.test.python.evaluation import TMP_DIR_PATH, XLSX_DATA_FOLDER
+from analysis.test.python.evaluation import XLSX_DATA_FOLDER
+from analysis.test.python.evaluation.utils.evaluation_configs import get_default_hyperstyle_config
 
 FILE_NAMES = [
     'test_wrong_column_name.xlsx',
@@ -17,15 +16,8 @@ FILE_NAMES = [
 @pytest.mark.parametrize('file_name', FILE_NAMES)
 def test_wrong_column(file_name: str):
     with pytest.raises(KeyError):
-        tmp_path = create_directory(TMP_DIR_PATH, clear=True)
-        config = HyperstyleEvaluationConfig(docker_path=None,
-                                            tool_path=HYPERSTYLE_RUNNER_PATH,
-                                            allow_duplicates=False,
-                                            with_all_categories=False,
-                                            new_format=True,
-                                            tmp_path=tmp_path)
-
         in_df = read_df(XLSX_DATA_FOLDER / file_name)
-        remove_directory(tmp_path)
-
+        config = get_default_hyperstyle_config(docker_path=None,
+                                               tool_path=HYPERSTYLE_RUNNER_PATH,
+                                               new_format=True)
         assert evaluate_hyperstyle(in_df, config)
