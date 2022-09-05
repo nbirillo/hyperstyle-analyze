af6c493512198e05bad283be224e4c6fc999cafb
tiginamaria <tiginamaria@gmail.com>
2022-09-05 18:45:15 +0200

    Add first simple test

--- analysis/test/python/data_analysis/templates/test_filter_template_issues.py
+++ analysis/test/python/data_analysis/templates/test_filter_template_issues.py
@@ -1,39 +1,26 @@
-from typing import List, Optional, Tuple, Union
+from typing import Optional, Union
 
 import pytest
 
 from analysis.src.python.data_analysis.model.column_name import SubmissionColumns
 from analysis.src.python.data_analysis.templates.filterring import filter_template_issues
-from analysis.src.python.data_analysis.templates.search import RepetitiveIssue, get_repetitive_issues
 from analysis.src.python.data_analysis.templates.utils.code_compare_utils import CodeComparator
-from analysis.src.python.utils.df_utils import read_df
+from analysis.src.python.utils.df_utils import equal_df, read_df, write_df
 from analysis.test.python.data_analysis import TEMPLATES_ISSUES_FOLDER
 
-REPETITIVE_ISSUES_FOLDER = TEMPLATES_ISSUES_FOLDER / 'repetitive_issues'
-SOLUTIONS_TEST_DATA = [
-    ('in_1_submission_series_python3_hyperstyle.csv',
-     ['e = 2.718281828459045',
-      '# put your python code here'],
+TEMPLATE_ISSUES_FOLDER = TEMPLATES_ISSUES_FOLDER / 'template_issues'
+
+TEMPLATE_ISSUES_TEST_DATA = [
+    ('template_issues_python3_hyperstyle.csv', 'submissions_python3_hyperstyle.csv', 'steps.csv',
      SubmissionColumns.HYPERSTYLE_ISSUES.value,
-     'edit_distance', False, False, 0, [RepetitiveIssue('WPS446', 0, 'e = 2.718281828459045', None, None)]),
-    ('in_2_submission_series_python3_hyperstyle.csv',
-     ['e = 2.718281828459045',
-      '# put your python code here'],
-     SubmissionColumns.HYPERSTYLE_ISSUES.value,
-     'edit_distance', False, False, 0, [RepetitiveIssue('WPS446', 0, 'e = 2.718281828459045', None, None),
-                                        RepetitiveIssue('WPS237', None, 'print(f"{e:.5f}")', None, None)]),
-    ('in_3_submission_series_python3_hyperstyle.csv',
-     ['e = 2.718281828459045',
-      '# put your python code here'],
-     SubmissionColumns.HYPERSTYLE_ISSUES.value,
-     'edit_distance', False, False, 0, []),
+     'edit_distance', False, False, 0, 'filtered_submissions_python3_hyperstyle.csv')
 ]
 
 
 @pytest.mark.parametrize(
-    ('submission_series_path', 'template_lines', 'issues_column', 'equal_type',
-     'ignore_trailing_comments', 'ignore_trailing_whitespaces', 'equal_upper_bound', 'repetitive_issues'),
-    SOLUTIONS_TEST_DATA,
+    ('templates_issues_path', 'submissions_path', 'steps_path', 'issues_column', 'equal_type',
+     'ignore_trailing_comments', 'ignore_trailing_whitespaces', 'equal_upper_bound', 'result_path'),
+    TEMPLATE_ISSUES_TEST_DATA,
 )
 def test_filter_template_issues(templates_issues_path: str,
                                 submissions_path: str,
@@ -43,23 +30,16 @@ def test_filter_template_issues(templates_issues_path: str,
                                 ignore_trailing_comments: bool,
                                 ignore_trailing_whitespaces: bool,
                                 equal_upper_bound: Optional[Union[int, float]],
-                                repetitive_issues: List[Tuple[str, int]]):
+                                result_path: str):
+    df_templates_issues = read_df(TEMPLATE_ISSUES_FOLDER / templates_issues_path)
+    df_steps = read_df(TEMPLATE_ISSUES_FOLDER / steps_path)
+    df_submissions = read_df(TEMPLATE_ISSUES_FOLDER / submissions_path)
+    code_comparator = CodeComparator(equal_type, ignore_trailing_comments, ignore_trailing_whitespaces,
+                                     equal_upper_bound)
 
-    df_templates_issues = read_df(REPETITIVE_ISSUES_FOLDER / templates_issues_path)
-    df_steps = read_df(REPETITIVE_ISSUES_FOLDER / steps_path)
-    df_submissions = read_df(REPETITIVE_ISSUES_FOLDER / submissions_path)
-    code_comparator = CodeComparator(equal_type, ignore_trailing_comments,
-                                     ignore_trailing_whitespaces, equal_upper_bound)
+    df_filtered_issues = filter_template_issues(df_templates_issues, df_submissions, df_steps, issues_column,
+                                                code_comparator)
+    write_df(df_filtered_issues, TEMPLATE_ISSUES_FOLDER / result_path)
 
-    filter_template_issues(df_templates_issues, df_submissions, df_steps, issues_column,
-                           code_comparator)
-
-    df_submission_series = read_df(REPETITIVE_ISSUES_FOLDER / submission_series_path)
-
-    code_comparator = CodeComparator(equal_type, ignore_trailing_comments,
-                                     ignore_trailing_whitespaces, equal_upper_bound)
-
-    actual_repetitive_issues = \
-        get_repetitive_issues(df_submission_series, template_lines, issues_column, code_comparator)
-
-    assert set(actual_repetitive_issues) == set(repetitive_issues)
+    df_result = read_df(TEMPLATE_ISSUES_FOLDER / result_path)
+    assert equal_df(df_filtered_issues, df_result)
