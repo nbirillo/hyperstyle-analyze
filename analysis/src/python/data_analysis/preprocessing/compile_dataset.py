8a7b77d6056db5d2ec571eb85489443d34e36be3
Maria Tigina <31625351+tiginamaria@users.noreply.github.com>
2022-09-14 10:55:55 +0200

    Python analysis (#46)
    * Add template issues filtration
    
    * Start to add tests
    
    * Fix filtration process
    
    * Adding tests
    
    * Add first simple test
    
    * Fix flake8
    
    * Add information about issues filtering to README.md
    
    * Add template issues filtration using diff + tests
    
    * Add new tests
    
    * Add README.md
    
    * Fix flake8
    
    * Fix flake8
    
    * Fix flake8
    
    * Add some fixes after qodana run on python dataset
    
    * Change docker version
    
    * Fix flake8
    
    * Add small fixes to preprocess datasets for analysis
    
    * Update analysis module
    
    * Fix CI
    
    * Delete filter file as we do not really need it

--- analysis/src/python/data_analysis/preprocessing/compile_dataset.py
+++ analysis/src/python/data_analysis/preprocessing/compile_dataset.py
@@ -2,13 +2,13 @@ import argparse
 import logging
 import sys
 
-from analysis.src.python.data_analysis.model.column_name import StepColumns, SubmissionColumns, TopicColumns, \
-    UserColumns
+from analysis.src.python.data_analysis.model.column_name import StepColumns, SubmissionColumns, TopicColumns
 from analysis.src.python.utils.df_utils import merge_dfs, read_df, write_df
+from analysis.src.python.utils.file_utils import create_directory, get_output_filename
 from analysis.src.python.utils.logging_utils import configure_logger
 
 
-def compile_dataset(submissions_path: str, steps_path: str, topics_path: str, users_path: str):
+def compile_dataset(submissions_path: str, steps_path: str, topics_path: str, result_path: str):
     """ Create directory with result dataset to analyse: select data which present in all datasets. """
 
     df_submissions = read_df(submissions_path)
@@ -17,12 +17,11 @@ def compile_dataset(submissions_path: str, steps_path: str, topics_path: str, us
     logging.info(f'Steps initial count: {df_steps.shape[0]}')
     df_topics = read_df(topics_path)
     logging.info(f'Topics initial size: {df_topics.shape[0]}')
-    df_users = read_df(users_path)
-    logging.info(f'Users initial size: {df_users.shape[0]}')
 
     df_compile = merge_dfs(df_submissions, df_steps, SubmissionColumns.STEP_ID.value, StepColumns.ID.value)
     df_compile = merge_dfs(df_compile, df_topics, StepColumns.TOPIC_ID.value, TopicColumns.ID.value)
-    df_compile = merge_dfs(df_compile, df_users, SubmissionColumns.USER_ID.value, UserColumns.ID.value)
+
+    logging.info(f'Compiled dataset shape: {df_compile.shape}')
 
     df_submissions = df_submissions[
         df_submissions[SubmissionColumns.ID.value].isin(df_compile[SubmissionColumns.ID.value])]
@@ -34,17 +33,13 @@ def compile_dataset(submissions_path: str, steps_path: str, topics_path: str, us
     df_topics = df_topics[df_topics[TopicColumns.ID.value].isin(df_compile[StepColumns.TOPIC_ID.value])]
     logging.info(f'Select {df_topics.shape[0]} topics which presented in compile dataset')
 
-    df_users = df_users[df_users[UserColumns.ID.value].isin(df_compile[SubmissionColumns.USER_ID.value])]
-    logging.info(f'Select {df_users.shape[0]} users which presented in compile dataset')
-
-    write_df(df_submissions, submissions_path)
+    result_path = create_directory(result_path)
+    write_df(df_submissions, result_path / get_output_filename(submissions_path))
     logging.info(f'Submissions final count: {df_submissions.shape[0]}')
-    write_df(df_steps, steps_path)
+    write_df(df_steps, result_path / get_output_filename(steps_path))
     logging.info(f'Steps final count: {df_steps.shape[0]}')
-    write_df(df_topics, topics_path)
+    write_df(df_topics, result_path / get_output_filename(topics_path))
     logging.info(f'Topics final count: {df_topics.shape[0]}')
-    write_df(df_users, users_path)
-    logging.info(f'Users final count: {df_users.shape[0]}')
 
 
 if __name__ == '__main__':
@@ -53,11 +48,12 @@ if __name__ == '__main__':
     parser.add_argument('submissions_path', type=str, help='Path to .csv file with submissions.')
     parser.add_argument('steps_path', type=str, help='Path to .csv file with steps.')
     parser.add_argument('topics_path', type=str, help='Path to .csv file with topics.')
-    parser.add_argument('users_path', type=str, help='Path to .csv file with users.')
+    parser.add_argument('result_path', type=str, help='Path to directory to save compiled datasets.')
+
     parser.add_argument('--log-path', type=str, default=None, help='Path to directory for log.')
 
     args = parser.parse_args(sys.argv[1:])
 
     configure_logger(args.submissions_path, 'compile', args.log_path)
 
-    compile_dataset(args.submissions_path, args.steps_path, args.topics_path, args.users_path)
+    compile_dataset(args.submissions_path, args.steps_path, args.topics_path, args.result_path)
